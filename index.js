8de01502f1e76366bc825e3c8eaea689d4e2cae1
mayur-sarang <mayur.68268@gmail.com>
2025-12-24 16:07:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from DevOps Practice! m');
+  res.end('Hello World from DevOps Practice! m s');
 });
 
 server.listen(3000, () => {
