1c56a3ebd78eedc129696b7e0ff8dafc07053491
mayur-sarang <mayur.68268@gmail.com>
2025-12-25 10:40:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from DevOps Practice!');
+  res.end('Hello World from DevOps Practice! hihih');
 });
 
 server.listen(3000, () => {
